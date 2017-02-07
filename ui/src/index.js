dbf5157ec6ac78d33cb6755242bcb3f91d74dd4c
Deonna Hodges <dnhodges@gmail.com>
2017-02-07 07:47:03 -0800

    refactor(ragency): Why am I using semicolons?

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Login from './login/Login';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Login from './login/Login'
+import './index.css'
 
 ReactDOM.render(
   <Login />,
   document.getElementById('root')
-);
+)
